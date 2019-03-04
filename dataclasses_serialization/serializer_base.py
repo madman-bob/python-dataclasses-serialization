12096f61d0c6d59540a349690961df2c610d5231
madman-bob <madman.bob@hotmail.co.uk>
2019-03-04 16:42:18 +0000

    Add basic Serializer class

---
+++ dataclasses_serialization/serializer_base.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass, fields, is_dataclass
+
+__all__ = [
+    "noop_serialization",
+    "noop_deserialization",
+    "dict_to_dataclass",
+    "Serializer",
+    "SerializationError",
+    "DeserializationError"
+]
+
+
+def noop_serialization(obj):
+    return obj
+
+
+def noop_deserialization(cls, obj):
+    if not isinstance(obj, cls):
+        raise DeserializationError("Cannot deserialize {} {!r} to type {}".format(
+            type(obj).__name__,
+            obj,
+            cls.__name__
+        ))
+
+    return obj
+
+
+def dict_to_dataclass(cls, dct, deserialization_func=noop_deserialization):
+    return cls(**{
+        fld.name: deserialization_func(fld.type, dct[fld.name])
+        for fld in fields(cls)
+        if fld.name in dct
+    })
+
+
+@dataclass
+class Serializer:
+    serialization_functions: dict
+    deserialization_functions: dict
+
+    def serialize(self, obj):
+        """
+        Serialize given Python object
+        """
+
+        for type_, func in self.serialization_functions.items():
+            if type_ is dataclass:
+                continue
+
+            if isinstance(obj, type_):
+                return func(obj)
+
+        if is_dataclass(obj) and dataclass in self.serialization_functions:
+            return self.serialization_functions[dataclass](obj)
+
+        raise SerializationError("Cannot serialize type {}".format(type(obj).__name__))
+
+    def deserialize(self, cls, serialized_obj):
+        """
+        Attempt to deserialize serialized object as given type
+        """
+
+        for type_, func in self.deserialization_functions.items():
+            if type_ is dataclass:
+                continue
+
+            if issubclass(cls, type_):
+                return func(cls, serialized_obj)
+
+        if is_dataclass(cls) and dataclass in self.deserialization_functions:
+            return self.deserialization_functions[dataclass](cls, serialized_obj)
+
+        raise DeserializationError("Cannot deserialize type {}".format(cls.__name__))
+
+
+class SerializationError(TypeError):
+    pass
+
+
+class DeserializationError(TypeError):
+    pass
---
+++ tests/test_serializer_base.py
@@ -0,0 +1,115 @@
+from dataclasses import dataclass, asdict
+from unittest import TestCase
+
+from dataclasses_serialization.serializer_base import (
+    noop_serialization, noop_deserialization,
+    dict_to_dataclass,
+    Serializer,
+    SerializationError, DeserializationError
+)
+
+
+class TestSerializerBase(TestCase):
+    def test_noop_serialization(self):
+        obj = object()
+        self.assertEqual(obj, noop_serialization(obj))
+
+    def test_noop_deserialization(self):
+        obj = object()
+
+        with self.subTest("Valid noop deserialization"):
+            self.assertEqual(obj, noop_deserialization(object, obj))
+
+        with self.subTest("Invalid noop deserialization"), self.assertRaises(DeserializationError):
+            noop_deserialization(int, obj)
+
+    def test_dict_to_dataclass_basic(self):
+        @dataclass
+        class ExampleDataclass:
+            int_field: int
+            str_field: str
+
+        self.assertEqual(
+            ExampleDataclass(1, "Hello, world"),
+            dict_to_dataclass(ExampleDataclass, {'int_field': 1, 'str_field': "Hello, world"})
+        )
+
+    def test_dict_to_dataclass_deserialization_func(self):
+        @dataclass
+        class ExampleDataclass:
+            int_field: int
+
+        with self.subTest("Invalid deserialization"), self.assertRaises(DeserializationError):
+            dict_to_dataclass(ExampleDataclass, {'int_field': "1"})
+
+        with self.subTest("Using deserialization func"):
+            self.assertEqual(
+                ExampleDataclass(1),
+                dict_to_dataclass(ExampleDataclass, {'int_field': "1"}, deserialization_func=lambda cls, obj: int(obj))
+            )
+
+    def test_serializer_serialization_basic(self):
+        int_serializer = Serializer({(int, str): int}, {})
+
+        with self.subTest("Serialize int -> int"):
+            self.assertEqual(1, int_serializer.serialize(1))
+
+        with self.subTest("Serialize str -> int"):
+            self.assertEqual(1, int_serializer.serialize("1"))
+
+        with self.subTest("Attempt to serialize object -> int"), self.assertRaises(SerializationError):
+            int_serializer.serialize(object())
+
+    def test_serializer_deserialization_basic(self):
+        int_deserializer = Serializer({}, {int: lambda cls, obj: str(obj)})
+
+        with self.subTest("Deserialize int -> str"):
+            self.assertEqual("1", int_deserializer.deserialize(int, 1))
+
+        with self.subTest("Attempt to deserialize object -> str"), self.assertRaises(DeserializationError):
+            int_deserializer.deserialize(str, object())
+
+    def test_serializer_dataclass_serialization(self):
+        @dataclass
+        class ExampleDataclass:
+            int_field: int
+
+        dataclass_serializer = Serializer({ExampleDataclass: lambda obj: obj.int_field}, {ExampleDataclass: lambda cls, obj: ExampleDataclass(obj)})
+
+        with self.subTest("Serialize"):
+            self.assertEqual(1, dataclass_serializer.serialize(ExampleDataclass(1)))
+
+        with self.subTest("Deserialize"):
+            self.assertEqual(ExampleDataclass(1), dataclass_serializer.deserialize(ExampleDataclass, 1))
+
+    def test_serializer_dataclass_fallback_serialization(self):
+        @dataclass
+        class ExampleDataclass:
+            int_field: int
+
+        @dataclass
+        class AnotherDataclass:
+            str_field: str
+
+        dataclass_serializer = Serializer(
+            {
+                ExampleDataclass: lambda obj: obj.int_field,
+                dataclass: lambda obj: asdict(obj)
+            },
+            {
+                ExampleDataclass: lambda cls, obj: ExampleDataclass(obj),
+                dataclass: lambda cls, obj: cls(**obj)
+            }
+        )
+
+        with self.subTest("Serialize known dataclass"):
+            self.assertEqual(1, dataclass_serializer.serialize(ExampleDataclass(1)))
+
+        with self.subTest("Serialize unknown dataclass"):
+            self.assertEqual({'str_field': "Hello, world"}, dataclass_serializer.serialize(AnotherDataclass("Hello, world")))
+
+        with self.subTest("Deserialize known dataclass"):
+            self.assertEqual(ExampleDataclass(1), dataclass_serializer.deserialize(ExampleDataclass, 1))
+
+        with self.subTest("Deserialize unknown dataclass"):
+            self.assertEqual(AnotherDataclass("Hello, world"), dataclass_serializer.deserialize(AnotherDataclass, {'str_field': "Hello, world"}))
