2368fa9d540c55d82afbab038891989fb14c715e
madman-bob <madman.bob@hotmail.co.uk>
2019-03-05 18:04:09 +0000

    Add basic BSONSerializer

---
+++ dataclasses_serialization/bson.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+
+from toolz import valmap
+
+from dataclasses_serialization.serializer_base import noop_serialization, noop_deserialization, dict_to_dataclass, Serializer
+
+try:
+    import bson
+
+    try:
+        # Assume py-bson version of bson installed
+        bson_loads = bson.loads
+        bson_dumps = bson.dumps
+
+    except AttributeError:
+        # Fallback to pymongo version of bson
+        bson_loads = lambda bson_str: bson.BSON(bson_str).decode()
+        bson_dumps = bson.BSON.encode
+
+except ImportError:
+    raise ImportError("bson module required for BSON serialization")
+
+__all__ = [
+    "BSONSerializer",
+    "BSONSerializerMixin",
+    "BSONStrSerializer",
+    "BSONStrSerializerMixin"
+]
+
+BSONSerializer = Serializer(
+    serialization_functions={
+        dataclass: lambda datacls: BSONSerializer.serialize(asdict(datacls)),
+        dict: lambda dct: valmap(BSONSerializer.serialize, dct),
+        list: lambda lst: list(map(BSONSerializer.serialize, lst)),
+        (str, int, float, datetime, bytes, bson.ObjectId, bool, type(None)): noop_serialization
+    },
+    deserialization_functions={
+        dataclass: lambda cls, serialized_obj: dict_to_dataclass(cls, serialized_obj, BSONSerializer.deserialize),
+        (dict, list, str, int, float, datetime, bytes, bson.ObjectId, bool, type(None)): noop_deserialization
+    }
+)
+
+
+class BSONSerializerMixin:
+    def as_bson(self):
+        return BSONSerializer.serialize(self)
+
+    @classmethod
+    def from_bson(cls, serialized_obj):
+        return BSONSerializer.deserialize(cls, serialized_obj)
+
+
+BSONStrSerializer = Serializer(
+    serialization_functions={
+        object: lambda obj: bson_dumps(BSONSerializer.serialize(obj))
+    },
+    deserialization_functions={
+        object: lambda cls, serialized_obj: BSONSerializer.deserialize(cls, bson_loads(serialized_obj))
+    }
+)
+
+
+class BSONStrSerializerMixin:
+    def as_bson_str(self):
+        return BSONStrSerializer.serialize(self)
+
+    @classmethod
+    def from_bson_str(cls, serialized_obj):
+        return BSONStrSerializer.deserialize(cls, serialized_obj)
---
+++ tests/test_bson.py
@@ -0,0 +1,113 @@
+from dataclasses import dataclass
+from datetime import datetime
+from unittest import TestCase, skipIf
+
+try:
+    import bson
+
+    bson_installed = True
+except ImportError:
+    bson_installed = False
+
+try:
+    from dataclasses_serialization.bson import BSONSerializer, BSONSerializerMixin, BSONStrSerializer, BSONStrSerializerMixin
+except ImportError:
+    BSONSerializer, BSONSerializerMixin, BSONStrSerializer, BSONStrSerializerMixin = [None] * 4
+
+
+@dataclass
+class Person:
+    name: str
+
+
+@dataclass
+class Song:
+    artist: Person
+
+
+@skipIf(not bson_installed, "BSON not installed")
+class TestBSON(TestCase):
+    def test_bson_serialization_basic(self):
+        obj = Person("Fred")
+        serialized_obj = {'name': "Fred"}
+
+        with self.subTest("Serialize dataclass -> BSON"):
+            self.assertEqual(serialized_obj, BSONSerializer.serialize(obj))
+
+        with self.subTest("Deserialize BSON -> dataclass"):
+            self.assertEqual(obj, BSONSerializer.deserialize(Person, serialized_obj))
+
+    def test_bson_serialization_types(self):
+        test_cases = [
+            (int, 1, 1),
+            (float, 1.0, 1.0),
+            (str, "Fred", "Fred"),
+            (datetime, datetime(2000, 1, 1), datetime(2000, 1, 1)),
+            (bytes, b'Hello, world', b'Hello, world'),
+            (bson.ObjectId, bson.ObjectId('0' * 24), bson.ObjectId('0' * 24)),
+            (bool, True, True),
+            (list, [], []),
+            (type(None), None, None)
+        ]
+
+        for type_, obj, serialized_obj in test_cases:
+            with self.subTest("Serialize object", obj=obj):
+                self.assertEqual(serialized_obj, BSONSerializer.serialize(obj))
+
+            with self.subTest("Deserialize object", obj=obj):
+                self.assertEqual(obj, BSONSerializer.deserialize(type_, serialized_obj))
+
+    def test_bson_serialization_nested(self):
+        obj = Song(Person("Fred"))
+        serialized_obj = {'artist': {'name': "Fred"}}
+
+        with self.subTest("Serialize nested dataclass -> BSON"):
+            self.assertEqual(serialized_obj, BSONSerializer.serialize(obj))
+
+        with self.subTest("Deserialize BSON -> nested dataclass"):
+            self.assertEqual(obj, BSONSerializer.deserialize(Song, serialized_obj))
+
+    def test_bson_serializer_mixin(self):
+        @dataclass
+        class Artist(BSONSerializerMixin):
+            name: str
+
+        obj = Artist("Fred")
+        serialized_obj = {'name': "Fred"}
+
+        with self.subTest("Serialize dataclass -> BSON with as_bson mixin"):
+            self.assertEqual(serialized_obj, obj.as_bson())
+
+        with self.subTest("Deserialize BSON -> dataclass with from_bson mixin"):
+            self.assertEqual(obj, Artist.from_bson(serialized_obj))
+
+    def test_bson_str_serialization(self):
+        obj = Person("Fred")
+        serialized_obj = b'\x14\x00\x00\x00\x02name\x00\x05\x00\x00\x00Fred\x00\x00'
+
+        with self.subTest("Serialize dataclass -> BSON string"):
+            self.assertEqual(serialized_obj, BSONStrSerializer.serialize(obj))
+
+        with self.subTest("Deserialize BSON string -> dataclass"):
+            self.assertEqual(obj, BSONStrSerializer.deserialize(Person, serialized_obj))
+
+    def test_bson_str_serializer_mixin(self):
+        @dataclass
+        class Artist(BSONStrSerializerMixin):
+            name: str
+
+        obj = Artist("Fred")
+        serialized_obj = b'\x14\x00\x00\x00\x02name\x00\x05\x00\x00\x00Fred\x00\x00'
+
+        with self.subTest("Serialize dataclass -> BSON string with as_bson_str mixin"):
+            self.assertEqual(serialized_obj, obj.as_bson_str())
+
+        with self.subTest("Deserialize BSON string -> dataclass with from_bson_str mixin"):
+            self.assertEqual(obj, Artist.from_bson_str(serialized_obj))
+
+
+@skipIf(bson_installed, "BSON installed")
+class TestBSONNotInstalled(TestCase):
+    def test_bson_raises_import_error(self):
+        with self.assertRaises(ImportError):
+            import dataclasses_serialization.bson
